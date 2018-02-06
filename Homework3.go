8e229b63a314629987b8f58789682e02452da445
rnatemiller <millern13@udayton.edu>
2018-02-05 21:59:54 -0500

    Function Struct layed out
    Addmsg, Clearlog, Getlog, and Savelog all now exist

--- Homework3.go
+++ Homework3.go
@@ -6,4 +6,29 @@ import (
 
 type Data_t struct {
 	Logged_time time.Time
+	Str         string
+}
+
+type Log_t struct {
+	item Data_t
+	next *Log_t
+}
+
+var headptr *Log_t
+var tailptr *Log_t
+
+func Addmsg(data Data_t) (int, error) {
+
+}
+
+func Clearlog() {
+
+}
+
+func Getlog() (string, error) {
+
+}
+
+func Savelog(filename string) error {
+
 }
